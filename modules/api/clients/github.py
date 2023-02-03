065947498283924aec75a579754c749d842a6c20
Aleks Khomenko <web.dev.alex72@gmail.com>
2023-02-03 13:46:52 +0200

    Add API tests for search repo in GitHub, ver.2.0

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -8,3 +8,12 @@ class GitHub:
         body = r.json()
 
         return body
+
+    def search_repo(self, name):
+        r = requests.get(
+            'https://api.github.com/search/repositories',
+            params={'q': name}
+        )
+        body = r.json()
+
+        return body
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -11,3 +11,22 @@ def test_user_exists(github_api):
 def test_user_not_exist(github_api):
     r = github_api.get_user('butenkosergii')
     assert r['message'] == 'Not Found'
+
+
+@pytest.mark.api
+def test_repo_can_be_found(github_api):
+    r = github_api.search_repo('become-qa-auto')
+    assert r['total_count'] == 26
+    assert 'become-qa-auto' in r['items'][0]['name']
+
+
+@pytest.mark.api
+def test_repo_cannot_be_found(github_api):
+    r = github_api.search_repo('sergiibutenko_repo_non_exist')
+    assert r['total_count'] == 0
+
+
+@pytest.mark.api
+def test_repo_with_single_char_be_found(github_api):
+    r = github_api.search_repo('s')
+    assert r['total_count'] != 0
