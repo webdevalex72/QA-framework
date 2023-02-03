6dde5531bbc6362c130d870efbd5052b73291f5e
Aleks Khomenko <web.dev.alex72@gmail.com>
2023-02-03 12:55:28 +0200

    Refactor testing API GitHub & add fixture ver.1.1

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -3,14 +3,8 @@ import requests
 
 class GitHub:
 
-    def get_user_defunkt(self):
-        r = requests.get('https://api.github.com/users/defunkt')
-        body = r.json()
-
-        return body
-
-    def get_non_exist_user(self):
-        r = requests.get('https://api.github.com/users/butenkosergii')
+    def get_user(self, username):
+        r = requests.get(f'https://api.github.com/users/{username}')
         body = r.json()
 
         return body
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -1,16 +1,13 @@
 import pytest
-from modules.api.clients.github import GitHub
 
 
 @pytest.mark.api
-def test_user_exists():
-    api = GitHub()
-    user = api.get_user_defunkt()
+def test_user_exists(github_api):
+    user = github_api.get_user('defunkt')
     assert user['login'] == 'defunkt'
 
 
 @pytest.mark.api
-def test_user_not_exist():
-    api = GitHub()
-    r = api.get_non_exist_user()
+def test_user_not_exist(github_api):
+    r = github_api.get_user('butenkosergii')
     assert r['message'] == 'Not Found'
